9328e761457544e63e89a193161bae6642bde6ec
Amato Luca <amatoroccoluca@gmail.com>
2024-10-26 16:56:46 +0200

    chore: add cart logic

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -10,10 +10,11 @@ const cartSlice = createSlice({
     addItemToCart: (state, action) => {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
 
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
@@ -24,7 +25,7 @@ const cartSlice = createSlice({
         existingItem.totalPrice = existingItem.totalPrice + existingItem.price;
       }
     },
-    removeItemFromCart: (state, payload) => {
+    removeItemFromCart: (state, action) => {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
